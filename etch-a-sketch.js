46298223541ad74104665e1885f4315a48173eb8
Mark Bailey <markbailey0356@gmail.com>
2018-09-12 16:16:56 +0930

    Make larger grid with configurable resolution

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,22 @@
-let grid = document.getElementsByClassName("grid")[0];
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  grid.appendChild(square);
-}
+const GRID_SIDE_LENGTH_IN_PIXELS = 960;
+const DEFAULT_GRID_SQUARES_PER_SIDE = 16;
+const grid = document.getElementsByClassName("grid")[0];
+grid.style.width = grid.style.height = GRID_SIDE_LENGTH_IN_PIXELS + "px";
+
+drawGrid(32);
+
 grid.addEventListener("mouseover", function(event) {
   if (event.target.classList.contains("square")) {
     event.target.classList.add("coloured");
   }
 });
+
+function drawGrid(squaresPerSide = DEFAULT_GRID_SQUARES_PER_SIDE) {
+  let squareSideLength = Math.floor(GRID_SIDE_LENGTH_IN_PIXELS / squaresPerSide);
+  for (let i = 0; i < Math.pow(squaresPerSide, 2); i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = square.style.height = squareSideLength + "px";
+    grid.appendChild(square);
+  }
+}
