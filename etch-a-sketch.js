542b2c4d68e0e38331320e4266d6132f4a5f1cba
Mark Bailey <markbailey0356@gmail.com>
2018-09-12 16:45:00 +0930

    Add redraw button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,9 @@
 const GRID_SIDE_LENGTH_IN_PIXELS = 960;
-const DEFAULT_GRID_SQUARES_PER_SIDE = 16;
+const DEFAULT_GRID_SQUARES_PER_SIDE = 32;
 const grid = document.getElementsByClassName("grid")[0];
 grid.style.width = grid.style.height = GRID_SIDE_LENGTH_IN_PIXELS + "px";
 
-drawGrid(32);
+drawGrid(DEFAULT_GRID_SQUARES_PER_SIDE);
 
 grid.addEventListener("mouseover", function(event) {
   if (event.target.classList.contains("square")) {
@@ -11,8 +11,17 @@ grid.addEventListener("mouseover", function(event) {
   }
 });
 
+const redrawButton = document.getElementsByClassName("redraw-button")[0];
+redrawButton.addEventListener("click", function(event) {
+  let newSquaresPerSide = prompt("Please enter how many squares per side", DEFAULT_GRID_SQUARES_PER_SIDE);
+  drawGrid(newSquaresPerSide);
+});
+
 function drawGrid(squaresPerSide = DEFAULT_GRID_SQUARES_PER_SIDE) {
-  let squareSideLength = Math.floor(GRID_SIDE_LENGTH_IN_PIXELS / squaresPerSide);
+  let squareSideLength = Math.floor(GRID_SIDE_LENGTH_IN_PIXELS / Math.floor(squaresPerSide));
+  while (grid.lastChild) {
+    grid.removeChild(grid.lastChild);
+  }
   for (let i = 0; i < Math.pow(squaresPerSide, 2); i++) {
     let square = document.createElement("div");
     square.classList.add("square");
